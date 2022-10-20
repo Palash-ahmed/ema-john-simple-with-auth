a1e2f1b1ce1eb109e0c300a13de43340488600e7
Palash Ahmed <palashbiochem@gmail.com>
2022-10-20 23:21:25 +0600

    ema john completed

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBJ7ADMpq175-WMruAmKtHKkk7y3cD_8k8",
+  authDomain: "ema-john-simple-83b93.firebaseapp.com",
+  projectId: "ema-john-simple-83b93",
+  storageBucket: "ema-john-simple-83b93.appspot.com",
+  messagingSenderId: "474374457302",
+  appId: "1:474374457302:web:009aed2845e1d315460677"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
